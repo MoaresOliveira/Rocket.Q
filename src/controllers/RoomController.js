639ad9fad025595ae654a5c841d1e76797a774ad
MoaresOliveira <moaresoliveira@gmail.com>
2021-06-26 16:12:28 -0300

    Implementei a verificação de salas no database para redirecionar pra sala se houvesse uma... E se não houvesse seria redirecionado para página de erro 404, criei as rotas e a view dá página e todo estilo. Na pagina de 404 a pessoa tem opção de voltar ao inicio ou pra tela de criação de Salas.

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,26 +1,27 @@
-const Database = require("../db/config")
+const Database = require("../db/config");
 
 module.exports = {
-    async create(req, res, vari=true){
-        const db = await Database()
-        const pass = req.body.password
-        let roomId = ''
-        let isRoom = true
+    async create(req, res, vari = true) {
+        const db = await Database();
+        const pass = req.body.password;
+        let roomId = "";
+        let isRoom = true;
 
-        while(isRoom){
-
+        while (isRoom) {
             // Gera o número da sala
-            for(let i = 0; i < 6; i++){
-                roomId += Math.floor(Math.random() * 10).toString()
+            for (let i = 0; i < 6; i++) {
+                roomId += Math.floor(Math.random() * 10).toString();
             }
 
             // Verifica se o número já existe
             const roomsExistIds = await db.all(`
                 SELECT id FROM rooms;
-            `)
-            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+            `);
+            isRoom = roomsExistIds.some(
+                (roomExistId) => roomExistId === roomId
+            );
             if (!isRoom) {
-                // Insere a sala no Banco 
+                // Insere a sala no Banco
                 await db.run(`
                     INSERT INTO rooms (
                         id, 
@@ -29,48 +30,64 @@ module.exports = {
                         ${parseInt(roomId)},
                         '${pass}'
                     )
-                `)
-                
+                `);
             }
-        }   
+        }
 
-        await db.close()
+        await db.close();
 
-        res.redirect(`/room/${roomId}`)
-        if(vari == false){
+        res.redirect(`/room/${roomId}`);
+        if (vari == false) {
             return roomId;
         }
-        
     },
 
-    async open(req, res){
-
+    async open(req, res) {
         const db = await Database();
-        
-        const roomId = req.params.room
-        
+
+        const roomId = req.params.room;
+
         const questions = await db.all(`
             SELECT * FROM questions WHERE room = ${roomId} and read = 0
-        `)
+        `);
 
         const questionsRead = await db.all(`
             SELECT * FROM questions WHERE room = ${roomId} and read = 1
-        `)
-        
-        let isNoQuestions
-        
-        if(questions.length == 0){
-            if(questionsRead.length == 0){
-                isNoQuestions = true
+        `);
+
+        let isNoQuestions;
+
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true;
             }
         }
 
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions});
+        res.render("room", {
+            roomId: roomId,
+            questions: questions,
+            questionsRead: questionsRead,
+            isNoQuestions: isNoQuestions,
+        });
     },
 
-    enter(req, res){
-        const roomId = req.body.roomId
+    async enter(req, res) {
+        const db = await Database();
+        const roomId = req.body.roomId;
+        let isRoom = true
 
-        res.redirect(`/room/${roomId}`)
-    }
-}
+        // Verifica se o número já existe
+        const roomsExistIds = await db.all(`
+                SELECT id FROM rooms;
+            `);
+        isRoom = roomsExistIds.some((roomExistId) => roomExistId === roomId);
+        if (isRoom) {
+            // Redireciona para a sala
+            res.redirect(`/room/${roomId}`);
+        }else{
+            // Redireciona para a sala de 404
+            res.redirect("/room-not-found")
+        }
+        
+    },
+};
--- src/route.js
+++ src/route.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const QuestionController = require('./controllers/QuestionController')
-const RoomController = require('./controllers/RoomController')
+const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
 
-const route = express.Router()
+const route = express.Router();
 
-route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
-route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+    res.render("index", { page: "create-pass" })
+);
 
-route.post('/create-room', RoomController.create)
-route.get('/room/:room', RoomController.open)
-route.post('/enterroom', RoomController.enter)
+route.post("/create-room", RoomController.create);
+route.get("/room/:room", RoomController.open);
+route.post("/enterroom", RoomController.enter);
+//Sala não encontrada
+route.get("/room-not-found", (req, res) =>
+    res.render("not-found", { error: "room-nonexist" })
+);
 
-route.post('/question/create/:room', QuestionController.create)
-route.post('/question/:room/:question/:action', QuestionController.index)
+route.post("/question/create/:room", QuestionController.create);
+route.post("/question/:room/:question/:action", QuestionController.index);
 
-module.exports = route
+module.exports = route;
