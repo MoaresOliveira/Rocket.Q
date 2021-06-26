5dc43c0b1b1706fdc8c7743408b52b2f8b2cec39
MoaresOliveira <moaresoliveira@gmail.com>
2021-06-26 19:01:03 -0300

    Refatoração do viewMode.js e alterações nas cores de alguns textos

--- public/scripts/viewMode.js
+++ public/scripts/viewMode.js
@@ -6,9 +6,7 @@ viewModeButton.addEventListener("click", function () {
     html.classList.toggle("dark");
     if(html.classList == ''){
         logo.setAttribute("src", "/images/logo.svg");
-        viewModeButton.innerHTML = '<i class="far fa-moon"></i>';
     }else {
         logo.setAttribute("src", "/images/logo-light.svg");
-        viewModeButton.innerHTML = '<i class="far fa-sun"></i>';        
     }
 });
