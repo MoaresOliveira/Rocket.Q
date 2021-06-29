b7b4c053678ec5f2caad1033e1582d5244b5aa49
MoaresOliveira <moaresoliveira@gmail.com>
2021-06-29 09:27:12 -0300

    Agora a escolha do usuário em relação ao Dark Mode, fica salvo no localStorage.

--- public/scripts/viewMode.js
+++ public/scripts/viewMode.js
@@ -2,11 +2,19 @@ const html = document.querySelector("html");
 const viewModeButton = document.querySelector("#viewMode");
 const logo = document.querySelector("header a img")
 
-viewModeButton.addEventListener("click", function () {
+if (localStorage.DarkMode) {
     html.classList.toggle("dark");
+    logo.setAttribute("src", "/images/logo-light.svg");
+    viewModeButton.checked = true
+}
+
+viewModeButton.addEventListener("click", function () {
+    html.classList.toggle("dark");    
     if(html.classList == ''){
         logo.setAttribute("src", "/images/logo.svg");
+        localStorage.setItem('DarkMode', false)
     }else {
         logo.setAttribute("src", "/images/logo-light.svg");
+        localStorage.setItem("DarkMode", true);
     }
 });
